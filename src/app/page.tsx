cf165c6b84795296026e9fb0b994558877625961
Leoodaviid <david_ax7@hotmail.com>
2023-03-22 18:55:42 -0300

    feat: componente NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Page() {
   return (
-    <div>
+    <div className="bg-primary-black overflow-hidden">
       <NavBar />
       <About />
       <Hero />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,27 @@
+'use client'
+import { motion } from 'framer-motion'
+import { navVariants } from '@/utils/motion'
+import styles from '../styles/index'
+import Image from 'next/image'
+import Search from '../public/search.svg'
+import Menu from '../public/menu.svg'
+
 const NavBar = () => {
-  return <nav>navbar</nav>
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial='hidden'
+      whileInView='show'
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className='absolute w-[50%] inset-0 gradient-01' />
+      <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+        <Image src={Search} alt='search' className='w-[24px] h-[24px] object-contain' />
+        <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>METAVERSUS</h2>
+        <Image src={Menu} alt='menu' className='w-[24px] h-[24px] objject-contain' />
+      </div>
+    </motion.nav>
+  )
 }
 
 export default NavBar

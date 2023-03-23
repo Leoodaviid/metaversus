63d2aec62a5a6cb52e6f1c76c01ae9010b9b162c
Leoodaviid <david_ax7@hotmail.com>
2023-03-23 20:23:10 -0300

    feat: Component insightCard, Insight

--- src/components/InsightCard.tsx
+++ src/components/InsightCard.tsx
@@ -1,7 +1,37 @@
 'use client'
+import { InsightsProps } from '@/models/models'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../utils/motion'
+import Image from 'next/image'
+import Arrow from '../public/arrow.svg'
 
-const InsightCard = () => {
-  return <div>Insight Card</div>
+interface InsightCardProps {
+  insight: InsightsProps
+}
+const InsightCard = ({ insight }: InsightCardProps) => {
+  return (
+    <motion.div
+      variants={fadeIn({ direction: 'up', type: 'spring', delay: insight.id * 0.5, duration: 1 })}
+      className='flex md:flex-row flex-col gap-4'
+    >
+      <Image
+        src={insight.imgUrl}
+        alt='planetas'
+        className='md:w-[270px] w-full h-[250px] rounded-[32px] object-cover'
+      />
+      <div className='w-full flex justify-between items-center'>
+        <div className='flex-1 md:ml-[62px] flex flex-col max-w-[650px]'>
+          <h4 className='font-normal lg:text-[42px] text-[26px] text-white'>{insight.title}</h4>
+          <p className='mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white'>
+            {insight.subtitle}
+          </p>
+        </div>
+        <div className='lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white'>
+          <Image src={Arrow} alt='seta' className='w-[40%] h-[40%] object-contain' />
+        </div>
+      </div>
+    </motion.div>
+  )
 }
 
 export default InsightCard
--- src/sections/Insights.tsx
+++ src/sections/Insights.tsx
@@ -1,6 +1,31 @@
 'use client'
+import { motion } from 'framer-motion'
+import { TypingText, TitleText } from '@/components/CustomTexts'
+import { staggerContainer } from '../utils/motion'
+import { insights } from '@/data'
+import styles from '../styles/index'
+import InsightCard from '@/components/InsightCard'
+
 const Insights = () => {
-  return <section>Insights section</section>
+  return (
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title='| Informações' textStyles='text-center' />
+        <TitleText title='Informações sobre o metaverso' textStyles='text-center' />
+        <div className='mt-[50px] flex flex-col gap-[30px]'>
+          {insights.map((insight) => (
+            <InsightCard key={`insight-${insight.id}`} insight={insight} />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  )
 }
 
 export default Insights
